a20f63ed23a212c69efd30e50d9cfa787d4ce75a
Daniel <danielpark0503@gmail.com>
2021-03-19 17:05:33 -0400

    postApi

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,6 +6,7 @@ import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
+import DiscoveryFeed from "./components/DiscoveryFeed";
 import { authenticate } from "./services/auth";
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       const user = await authenticate();
       if (!user.errors) {
         setAuthenticated(true);
@@ -37,16 +38,34 @@ function App() {
           />
         </Route>
         <Route path="/sign-up" exact={true}>
-          <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
+          <SignUpForm
+            authenticated={authenticated}
+            setAuthenticated={setAuthenticated}
+          />
         </Route>
-        <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-          <UsersList/>
+        <ProtectedRoute
+          path="/users"
+          exact={true}
+          authenticated={authenticated}
+        >
+          <UsersList />
         </ProtectedRoute>
-        <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
+        <ProtectedRoute
+          path="/users/:userId"
+          exact={true}
+          authenticated={authenticated}
+        >
           <User />
         </ProtectedRoute>
         <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
           <h1>My Home Page</h1>
+        </ProtectedRoute>
+        <ProtectedRoute
+          path="/discoveryfeed"
+          exact={true}
+          authenticated={authenticated}
+        >
+          <DiscoveryFeed />
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
---
+++ react-app/src/components/DiscoveryFeed.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { createApi } from "unsplash-js";
+// const mode = process.env.NODE_ENV;
+// const apiKey = process.env.ACCESS_KEY;
+
+const photosApi = createApi({
+  accessKey: "az_UO2AxnG4t9EGPDqXAGshoAUSKG-a83fAF9BpulZo",
+});
+
+const FetchingPhoto = ({ photo }) => {
+  const { urls, user } = photo;
+
+  return (
+    <>
+      <img src={urls.regular} />
+      <div>{user.name}</div>
+    </>
+  );
+};
+
+const DiscoveryFeed = () => {
+  const [photos, setPhotos] = useState(null);
+
+  useEffect(() => {
+    photosApi.photos
+      .list({ order_by: "popular" })
+      .then((result) => {
+        setPhotos(result);
+      })
+      .catch(() => {
+        console.log("Error! Try again!");
+      });
+  }, []);
+
+  if (photos === null) {
+    return <div>Please wait..</div>;
+  } else {
+    return (
+      <div>
+        <ul>
+          {photos.response.results.map((photo) => (
+            <li key={photo.id}>
+              <FetchingPhoto photo={photo} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+};
+
+export default DiscoveryFeed;
