2da25c2766dd8a0af9b7d7ce7131b2a9a809c7b9
Kevin <pitzer.kevinj@gmail.com>
2021-03-21 16:39:57 -0400

    mess with seeder

---
+++ app/seeds/follows.py
@@ -0,0 +1,19 @@
+from app.models import db, Follow
+
+# add users in range to Demo's followers
+def seed_follows():
+
+  for i in range(2, 35):
+    new_follower = Follow(
+      followedId = 1,
+      followerId = i
+    )
+    db.session.add(new_follower)
+  
+  db.session.commit()
+
+
+
+def undo_follows():
+  db.session.execute('TRUNCATE follows;')
+  db.session.commit()
