3e91cb0d8351bed3156bb56a3922f43c9900f38d
keitay72 <70104445+keitay72@users.noreply.github.com>
2021-03-22 15:51:44 -0500

    follow query

--- react-app/src/store/follow.js
+++ react-app/src/store/follow.js
@@ -1,12 +1,38 @@
 const IS_FOLLOWING = "IS_FOLLOWING"
 const FOLLOW_USER = "FOLLOW_USER"
 const UNFOLLOW_USER = "UNFOLLOW_USER"
+const IS_FOLLOWED_BY = "IS_FOLLOWED_BY"
 
-const followers = (following) => ({
+
+const loadFollowers = (following) => ({
     type = IS_FOLLOWING,
     following,
 })
 
-// export const getAllFollowers = () => {
 
-// }
+const loadFollowedBy = (followed_by) => ({
+    type = IS_FOLLOWED_BY,
+    followed_by,
+})
+
+export const getAllFollowers = (userId) => async dispatch => {
+    const response = await fetch(`/api/follow/${userId}/follows`)
+
+    if (response.ok) {
+        const followers = await response.json()
+        dispatch(loadFollowers(followers))
+    }
+
+    return response
+}
+
+export const getAllFollowedBy = (userId) => async dispatch => {
+    const response = await fetch(`/api/follow/${userId}/followedBy`)
+
+    if (response.ok) {
+        const followedBy = await response.json()
+        dispatch(loadFollowedBy(followedBy))
+    }
+
+    return response
+}
