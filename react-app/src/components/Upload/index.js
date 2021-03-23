fb5edc5c30690fc0d7d0dd57b4bf93cdf42329ad
Kevin <pitzer.kevinj@gmail.com>
2021-03-23 11:17:01 -0400

    upload image package

--- react-app/src/components/Upload/index.js
+++ react-app/src/components/Upload/index.js
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import ImageUploading from 'react-images-uploading';
 
 import './Upload.css'
 
 const Upload = () => {
+  const [image, setImage] = useState([]);
+  const maxNumber = 69;
+
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImage(imageList);
+  };
 
   return (
-    <h1>Upload your shit here</h1>
+    <div className="upload">
+      <h1>Upload your shit here</h1>
+      <ImageUploading
+        multiple
+        value={image}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          // write your building UI
+          <div className="upload__image-wrapper">
+            <button
+              style={isDragging ? { color: 'red' } : undefined}
+              onClick={onImageUpload}
+              {...dragProps}
+            >
+              Click or Drop here
+            </button>
+            &nbsp;
+            <button onClick={onImageRemoveAll}>Remove all images</button>
+            {imageList.map((image, index) => (
+              <div key={index} className="image-item">
+                <img src={image['data_url']} alt="" width="100" />
+                <div className="image-item__btn-wrapper">
+                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ImageUploading>
+    </div>
   )
 }
 
