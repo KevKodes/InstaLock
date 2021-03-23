40e857df9af8055c5ed4dc15e97bb2b930db379d
Kevin <pitzer.kevinj@gmail.com>
2021-03-23 15:56:32 -0400

    logging the new post data

--- react-app/src/components/Upload/index.js
+++ react-app/src/components/Upload/index.js
@@ -1,21 +1,67 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from 'react-router-dom'
 import ImageUploading from 'react-images-uploading';
 
 import './Upload.css'
 
 const Upload = () => {
   const [image, setImage] = useState([]);
-  const maxNumber = 69;
+  const [caption, setCaption] = useState('');
+  const [vaulted, setVaulted] = useState(false);
+  const sessionUser = useSelector(state => state.session.user)
+  const maxNumber = 1;
+  // console.log('image url: ', image[0]?.data_url)
+  // console.log('vaulted: ', vaulted)
 
   const onChange = (imageList, addUpdateIndex) => {
     // data for submit
-    console.log(imageList, addUpdateIndex);
+    // console.log('image: ', imageList, 'addUpdateIndex: ', addUpdateIndex);
     setImage(imageList);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // validate the post
+
+    // add the post to the database
+    const newPost = {
+      userId: sessionUser.id,
+      photoURL: image[0]?.data_url,
+      caption,
+      vaulted,
+    }
+    console.log(newPost)
+    
+    // return <Redirect to="/" />
+  }
+
+  const imageDiv = image[0]?.data_url && (
+    <div className="image-test">
+      <img src={`${image[0].data_url}`} alt="upload" />
+      <form onSubmit={handleSubmit}>
+        <h3>Add a caption for your post</h3>
+        <input 
+          type="text"
+          placeholder="Caption"
+          value={caption}
+          onChange={e => setCaption(e.target.value)}
+        ></input>
+        <label for="vaulted">Vault my photo</label>
+        <input
+          type="checkbox"
+          name="vaulted"
+          checked={vaulted}
+          onChange={e => setVaulted(e.target.checked)}
+        ></input>
+        <button type="submit">Upload Photo</button>
+      </form>
+    </div>
+  )
+
   return (
     <div className="upload">
-      <h1>Upload your shit here</h1>
+      <h1>Upload a picture to share with friends</h1>
       <ImageUploading
         multiple
         value={image}
@@ -41,11 +87,10 @@ const Upload = () => {
             >
               Click or Drop here
             </button>
-            &nbsp;
-            <button onClick={onImageRemoveAll}>Remove all images</button>
+            {/* <button onClick={onImageRemoveAll}>Remove all images</button> */}
             {imageList.map((image, index) => (
               <div key={index} className="image-item">
-                <img src={image['data_url']} alt="" width="100" />
+                <img src={image['data_url']} alt="" width="200" />
                 <div className="image-item__btn-wrapper">
                   <button onClick={() => onImageUpdate(index)}>Update</button>
                   <button onClick={() => onImageRemove(index)}>Remove</button>
@@ -55,6 +100,7 @@ const Upload = () => {
           </div>
         )}
       </ImageUploading>
+      {imageDiv}
     </div>
   )
 }
