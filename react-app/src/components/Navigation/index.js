aeb94ed4a9c2e6be1c0a5f4808ac6454068b23f5
Robert Vogtritter <RCVogtritter@gmail.com>
2021-03-24 14:18:53 -0700

    css

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -25,33 +25,38 @@ const NavBar = ({ setAuthenticated, userName }) => {
         </div>
         <ul id="nav">
           <li>
-
             {/* Added className navIcon */}
-            <NavLink id="ez" to="/discoveryfeed" exact={true} className="navIcon" activeClassName="active"></NavLink>
-          </li>
-          <li>
-
-            {/* Added className navIcon */}
-            <NavLink id="ez2" to="/" exact={true} className="navIcon" activeClassName="active">
-
-
-            </NavLink>
+            <div className="tooltip">
+              <NavLink id="ez" to="/discoveryfeed" exact={true} className="navIcon" activeClassName="active"></NavLink>
+              <span class="tooltiptext">Explore</span>
+            </div>
           </li>
           <li>
             {/* Added className navIcon */}
-            <NavLink id="ez3" to="/upload" exact={true} className="navIcon" activeClassName="active">
-
-            </NavLink>
+            <div className="tooltip">
+              <NavLink id="ez2" to="/" exact={true} className="navIcon" activeClassName="active"></NavLink>
+              <span class="tooltiptext2">Home</span>
+            </div>
           </li>
           <li>
-
+            {/* Added className navIcon */}
+            <div className="tooltip">
+              <NavLink id="ez3" to="/upload" exact={true} className="navIcon" activeClassName="active"></NavLink>
+              <span class="tooltiptext2">Upload</span>
+            </div>
+          </li>
+          <li>
             {/* Added className navIcon*/}
-            <NavLink id="ez5" to={`/${userName}`} exact={true} className="navIcon" activeClassName="active">
-
-            </NavLink>
+            <div className="tooltip">
+              <NavLink id="ez5" to={`/${userName}`} exact={true} className="navIcon" activeClassName="active"></NavLink>
+              <span class="tooltiptext2">Profile</span>
+            </div>
           </li>
           <li>
+          <div className="tooltip">
             <LogoutButton setAuthenticated={setAuthenticated} />
+            <span class="tooltiptext">Logout</span>
+          </div>
           </li>
         </ul>
       </nav>
