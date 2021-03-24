a613d439efe22cc73164855f9b83b9a2bd98d575
Daniel <danielpark0503@gmail.com>
2021-03-24 16:12:30 -0400

    onePhotoPerUser

--- migrations/versions/9ea4dd097318_update.py
+++ migrations/versions/9ea4dd097318_update.py
@@ -1,8 +1,8 @@
-"""new table
+"""update
 
-Revision ID: 7c70171ae1e7
+Revision ID: 9ea4dd097318
 Revises: 
-Create Date: 2021-03-24 01:14:01.366102
+Create Date: 2021-03-24 11:07:41.540037
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '7c70171ae1e7'
+revision = '9ea4dd097318'
 down_revision = None
 branch_labels = None
 depends_on = None
