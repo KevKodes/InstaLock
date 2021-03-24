13254e18c3319a26b543464d8427751f058bff29
keitay72 <70104445+keitay72@users.noreply.github.com>
2021-03-24 10:15:19 -0500

    worked on discovery

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -28,69 +28,69 @@ const SignUpForm = ({ authenticated, setAuthenticated }) => {
     <div className="Big">
       <div className="flag1 flag"></div>
       <div className="flag2 flag"></div>
-    <div className="signup-form">
-      <form onSubmit={onSignUp}>
-        <h1>InstaLock</h1>
-        <h2>Sign up to see photos and videos from your friends.</h2>
-        <div>
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            onChange={e => setEmail(e.target.value)}
-            value={email}
-          ></input>
-        </div>
-        <div>
-          <input
-            name="firstName"
-            type="text"
-            placeholder="First Name"
-            value={firstName}
-            onChange={e => setFirstName(e.target.value)}
-          />
-        </div>
-        <div>
-          <input
-            name="lastName"
-            type="text"
-            placeholder="Last Name"
-            value={lastName}
-            onChange={e => setLastName(e.target.value)}
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            onChange={e => setUsername(e.target.value)}
-            value={username}
-          ></input>
-        </div>
-        <div>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={e => setPassword(e.target.value)}
-            value={password}
-          ></input>
-        </div>
-        <button type="submit">Sign up</button>
-      </form>
-      <div className="login-link-box">
-        <div className="login-link-text">
-          Have an account?
-          <NavLink to="/login">
-            Log in
+      <div className="signup-form">
+        <form onSubmit={onSignUp}>
+          <h1>InstaLock</h1>
+          <h2>Sign up to see photos and videos from your friends.</h2>
+          <div>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={e => setEmail(e.target.value)}
+              value={email}
+            ></input>
+          </div>
+          <div>
+            <input
+              name="firstName"
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              name="lastName"
+              type="text"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={e => setLastName(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={e => setUsername(e.target.value)}
+              value={username}
+            ></input>
+          </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={e => setPassword(e.target.value)}
+              value={password}
+            ></input>
+          </div>
+          <button type="submit">Sign up</button>
+        </form>
+        <div className="login-link-box">
+          <div className="login-link-text">
+            Have an account?&nbsp;
+          <NavLink to="/login" style={{ textDecoration: 'none' }}>
+              Log in
           </NavLink>
+          </div>
         </div>
+        <div className="banner1"></div>
       </div>
-      <div className="banner1"></div>
-    </div>
-    <div className="flag3 flag"></div>
-    <div className="flag4 flag"></div>
+      <div className="flag3 flag"></div>
+      <div className="flag4 flag"></div>
     </div>
   );
 };
