6d2f103b5546e3de930177a71fa0852656cef996
Robert Vogtritter <RCVogtritter@gmail.com>
2021-04-29 13:40:52 -0700

    search bar works completely

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,178 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from 'react-redux';
-import { Link, NavLink, Redirect, useHistory } from "react-router-dom";
+import { Link, NavLink, useHistory } from "react-router-dom";
 import LogoutButton from "../auth/LogoutButton";
 import "../../index.css"
 
 
 const NavBar = ({ setAuthenticated }) => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch(`/api/users/`).then(response => response.json().then(data=> {
+      let usernameArray = []
+      const names = Object.values(data)
+      const userinfo = names[0]
+      const supervalues = Object.values(userinfo)
+
+      for(let i = 0; i < supervalues.length; i++){
+        const valuepoint = Object.values(supervalues[i])
+        usernameArray.push(valuepoint[5])
+      }
+      setUsers(usernameArray)
+
+    }))
+  }, [])
+
 
   let history = useHistory();
   const handleSubmit = (that) => {
-    const nameList = [
-      "Aatrox",
-      "Ahri",
-      "Akali",
-      "Alistar",
-      "Amumu",
-      "Anivia",
-      "Annie",
-      "Aphelios",
-      "Ashe",
-      "Aurelion-Sol",
-      "Azir",
-      "Bard",
-      "Blitzcrank",
-      "Brand",
-      "Braum",
-      "Caitlyn",
-      "Camille",
-      "Cassiopeia",
-      "Cho'Gath",
-      "Corki",
-      "Darius",
-      "Diana",
-      "Dr.Mundo",
-      "Draven",
-      "Ekko",
-      "Elise",
-      "Evelynn",
-      "Ezreal",
-      "Fiddlesticks",
-      "Fiora",
-      "Fizz",
-      "Galio",
-      "Gangplank",
-      "Garen",
-      "Gnar",
-      "Gragas",
-      "Graves",
-      "Hecarim",
-      "Heimerdinger",
-      "Illaoi",
-      "Irelia",
-      "Ivern",
-      "Janna",
-      "JarvanIV",
-      "Jax",
-      "Jayce",
-      "Jhin",
-      "Jinx",
-      "Kai'Sa",
-      "Kalista",
-      "Karma",
-      "Karthus",
-      "Kassadin",
-      "Katarina",
-      "Kayle",
-      "Kayn",
-      "Kennen",
-      "Kha'Zix",
-      "Kindred",
-      "Kled",
-      "Kog'Maw",
-      "LeBlanc",
-      "Lee-Sin",
-      "Leona",
-      "Lillia",
-      "Lissandra",
-      "Lucian",
-      "Lulu",
-      "Lux",
-      "Malphite",
-      "Malzahar",
-      "Maokai",
-      "Master Yi",
-      "Miss Fortune",
-      "Mordekaiser",
-      "Morgana",
-      "Nami",
-      "Nasus",
-      "Nautilus",
-      "Neeko",
-      "Nidalee",
-      "Nocturne",
-      "Nunu",
-      "Olaf",
-      "Orianna",
-      "Ornn",
-      "Pantheon",
-      "Poppy",
-      "Pyke",
-      "Qiyana",
-      "Quinn",
-      "Rakan",
-      "Rammus",
-      "Rek'Sai",
-      "Rell",
-      "Renekton",
-      "Rengar",
-      "Riven",
-      "Rumble",
-      "Ryze",
-      "Samira",
-      "Sejuani",
-      "Senna",
-      "Seraphine",
-      "Sett",
-      "Shaco",
-      "Shen",
-      "Shyvana",
-      "Singed",
-      "Sion",
-      "Sivir",
-      "Skarner",
-      "Sona",
-      "Soraka",
-      "Swain",
-      "Sylas",
-      "Syndra",
-      "Tahm-Kench",
-      "Taliyah",
-      "Talon",
-      "Taric",
-      "Teemo",
-      "Thresh",
-      "Tristana",
-      "Trundle",
-      "Tryndamere",
-      "Twisted Fate",
-      "Twitch",
-      "Udyr",
-      "Urgot",
-      "Varus",
-      "Vayne",
-      "Veigar",
-      "Vel'Koz",
-      "Vi",
-      "Viktor",
-      "Vladimir",
-      "Volibear",
-      "Warwick",
-      "Wukong",
-      "Xayah",
-      "Xerath",
-      "Xin-Zhao",
-      "Yasuo",
-      "Yone",
-      "Yorick",
-      "Yuumi",
-      "Zac",
-      "Zed",
-      "Ziggs",
-      "Zilean",
-      "Zoe",
-      "Zyra"
-
-    ]
     let drop = that.toLowerCase()
     let update = drop[0].toUpperCase() + drop.substring(1)
-    if (nameList.includes(update)){
+    if (users.includes(update)){
       return history.push(`/${update}`)
     }
     else {
       alert("user doesn't exist")
     }
+
   }
   const sessionUser = useSelector((state) => state?.session?.user);
 
